c145660d0bf2f80629351a62255b6f048ea3c5ef
Jon San Miguel <sanmiguelje@gmail.com>
2017-03-23 17:50:58 -0400

    Use ascii encoding

--- cecho.sh
+++ cecho.sh
@@ -10,34 +10,34 @@ cecho()
 {
 	#FOREGROUND
 
-	local FG_BLACK="\e[30m"
-	local FG_RED="\e[31m"
-	local FG_GREEN="\e[32m"
-	local FG_YELLOW="\e[33m"
-	local FG_BLUE="\e[34m"
-	local FG_PURPLE="\e[35m"
-	local FG_CYAN="\e[36m"
-	local FG_WHITE="\e[37m"
+    local FG_BLACK="\033[0;30m"
+    local FG_RED="\033[0;31m"
+    local FG_GREEN="\033[0;32m"
+    local FG_YELLOW="\033[0;33m"
+    local FG_BLUE="\033[0;34m"
+    local FG_PURPLE="\033[0;35m"
+    local FG_CYAN="\033[0;36m"
+    local FG_WHITE="\033[0;37m"
 
-	#BACKGROUND
+    #BACKGROUND
 
-	local BG_BLACK="\e[40m"
-	local BG_RED="\e[41m"
-	local BG_GREEN="\e[42m"
-	local BG_YELLOW="\e[43m"
-	local BG_BLUE="\e[44m"
-	local BG_PURPLE="\e[45m"
-	local BG_CYAN="\e[46m"
-	local BG_WHITE="\e[47m"
+    local BG_BLACK="\033[0;40m"
+    local BG_RED="\033[0;41m"
+    local BG_GREEN="\033[0;42m"
+    local BG_YELLOW="\033[0;43m"
+    local BG_BLUE="\033[0;44m"
+    local BG_PURPLE="\033[0;45m"
+    local BG_CYAN="\033[0;46m"
+    local BG_WHITE="\033[0;47m"
 
-	#ACTION
+    #ACTION
 
-	local DONE="\e[0m"
-	local HIGHLIGHT="\e[1m"
-	local UNDERLINE="\e[4m"
-	local BLINK="\e[5m"
-	local REVERSE="\e[7m"
-	local INVISIBLE="\e[8m"
+    local DONE="\033[0;0m"
+    local HIGHLIGHT="\033[0;1m"
+    local UNDERLINE="\033[0;4m"
+    local BLINK="\033[0;5m"
+    local REVERSE="\033[0;7m"
+    local INVISIBLE="\033[0;8m"
 
 	# for Bash compatibility
 
